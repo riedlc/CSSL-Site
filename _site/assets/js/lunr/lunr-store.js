15e883941d303db181f74d2b638b6387f0c90558
Christoph Riedl <c.riedl@neu.edu>
2018-04-27 16:18:22 -0400

    Fixing localhost links

--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -2,5 +2,5 @@ var store = [{
         "title": "This is a first post",
         "excerpt":"About the lab. ","categories": [],
         "tags": [],
-        "url": "http://localhost:4000/test/",
+        "url": "https://christophriedl.net/test/",
         "teaser":null}]

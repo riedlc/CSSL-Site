f65a6f70af53d7d59344b8716101d3700bdc9e92
Christoph Riedl <c.riedl@neu.edu>
2018-04-29 11:06:49 -0400

    Added html extension to menu links

--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -2,5 +2,5 @@ var store = [{
         "title": "This is a first post",
         "excerpt":"About the lab. ","categories": [],
         "tags": [],
-        "url": "https://christophriedl.net/test/",
+        "url": "https://christophriedl.net/test.html",
         "teaser":null}]
